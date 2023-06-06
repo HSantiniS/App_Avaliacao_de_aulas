d96661bbce4def777b1756a6b48f8abbc2068690
HSantiniS <hsantini.05+github@gmail.com>
2023-06-06 17:02:27 -0300

    segundo commit

--- AppAvaliacoes/MainPage.xaml.cs
+++ AppAvaliacoes/MainPage.xaml.cs
@@ -7,4 +7,15 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		BindingContext = viewModel;
 	}
+
+	//Não está funcionando --> mudar página
+	private async Task OnSignUpLabelClickedAsync( EventArgs e)
+	{
+        await Shell.Current.GoToAsync("SignUpPage");
+    }
+
+    private async Task EntrarBtn_ClickedAsync(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("PaAvaliationsPage");
+    }
 }
